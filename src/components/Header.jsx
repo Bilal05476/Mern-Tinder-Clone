707c7472455fee4ce007f73eb99401ef2f97abf8
bilal05476 <ba4762313@gmail.com>
2021-03-05 16:36:41 +0500

    updates

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import "./css/Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from "@material-ui/icons/Forum";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
+      <img
+        src="https://logos-world.net/wp-content/uploads/2020/09/Tinder-Emblem.png"
+        alt="logo"
+        className="header__logo"
+      />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
+      </IconButton>
+    </div>
+  );
+};
+
+export default Header;
